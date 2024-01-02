ec64d2b89c57c5452a0fbc04c0ff8365bc640bd0
ludwig <sigwartholiver2@gmail.com>
2024-01-01 22:21:12 -0600

    Begin functionality
    Took 21 seconds

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+app.use(express.static(__dirname));
+
+app.get("/search", (req, res) => {
+  console.log("Received a search request");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
