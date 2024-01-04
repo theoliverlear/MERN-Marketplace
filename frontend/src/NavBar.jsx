26e09f89a6eb9300e18540d11c5b06815b9886f7
ludwig <sigwartholiver2@gmail.com>
2024-01-03 22:19:06 -0600

    Replace 'a' tags with 'Link' in NavBar
    Took 19 seconds

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,29 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 import logo from "./images/logo.png";
+
 function NavBar() {
     return (
         <div className="NavBar">
-
             <nav>
                 <img src={logo} id="nav-logo-img"
                      alt="logo"/>
                 <ul>
                     <li>
-                        <a className="nav-list-link" href="/"><h3>Home</h3>
-                        </a>
+                        <Link className="nav-list-link" to="/">
+                            <h3>Home</h3>
+                        </Link>
                     </li>
                     <li>
-                        <a className="nav-list-link" href="/explore">
-                            <h3>Explore</h3></a>
+                        <Link className="nav-list-link" to="/explore">
+                            <h3>Explore</h3>
+                        </Link>
                     </li>
                     <li>
-                        <a className="nav-list-link" href="/search">
-                            <h3>Search</h3></a>
+                        <Link className="nav-list-link" to="/search">
+                            <h3>Search</h3>
+                        </Link>
                     </li>
                     <li>
-                        <a className="nav-list-link" href="/about">
-                            <h3>About</h3></a>
+                        <Link className="nav-list-link" to="/about">
+                            <h3>About</h3>
+                        </Link>
                     </li>
                 </ul>
             </nav>
