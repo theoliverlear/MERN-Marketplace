5825f7b9a4264f40b5a8e8242340bd8e7c82d771
ludwig <sigwartholiver2@gmail.com>
2024-01-03 22:19:44 -0600

    Add routing
    Took 27 seconds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Header from "./Header";
+import React from "react";
+import Home from "./Home";
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from "react-router-dom";
 function App() {
-  return (
-      <div>
-        <Header />
-      </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
