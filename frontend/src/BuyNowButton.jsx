fb389267e2a9b68877f486cbb615e58a0086a172
ludwig <sigwartholiver2@gmail.com>
2024-01-04 20:50:42 -0600

    Add file

---
+++ frontend/src/BuyNowButton.jsx
@@ -0,0 +1,11 @@
+import "./BuyNowButton.css";
+function BuyNowButton() {
+    return (
+        <div className="BuyNowButton">
+            <button className="buy-now-button">
+                Buy now
+            </button>
+        </div>
+    );
+}
+export default BuyNowButton;
