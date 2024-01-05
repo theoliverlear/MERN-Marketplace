a25d959da5028398498b483ec368f3ca95832796
theoliverlear <sigwartholiver2@gmail.com>
2024-01-05 15:12:47 -0600

    Update import locations
    Took 41 seconds

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import './colors.css';
+import App from './components/app/App';
+import './components/colors/colors.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
